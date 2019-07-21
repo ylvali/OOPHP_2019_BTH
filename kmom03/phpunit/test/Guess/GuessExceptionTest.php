b668bb76079ae7a2dc3b2f034215c7f2e54d37a3
Ylvali <ylva@spektatum.com>
2019-07-21 13:19:54 +0100

    Testcases with 100% coverage done

---
+++ kmom03/phpunit/test/Guess/GuessExceptionTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Ylva\Guess;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Test cases for class Guess.
+ */
+class GuessCreateObjectTestException extends TestCase
+{
+
+  /**
+  *
+  * Construct object & test the exception thrown
+  *
+  */
+
+  // SOLUTION FROM: https://thephp.cc/news/2016/02/questioning-phpunit-best-practices
+
+  /**
+     *
+     * @expectedException \Ylva\Guess\GuessException
+     *
+     *
+     */
+    public function testExpectedExceptionIsRaised()
+    {
+        $guess = new Guess(7, 1);
+        $this->assertInstanceOf("\Ylva\Guess\Guess", $guess);
+
+        $exc = new GuessException();
+        $this->assertInstanceOf("\Ylva\Guess\GuessException", $exc);
+
+        $guess->makeGuess(200);
+    }
+}
