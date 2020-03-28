681eba6583baf23423e88b4406c01afae65b5c44
Ylvali <ylva@spektatum.com>
2020-03-28 06:49:22 +0100

    databases and pdo added

---
+++ kmom05/example/db/insert.sql
@@ -0,0 +1,10 @@
+-- Delete all from before
+
+DELETE FROM test;
+--
+-- Add teacher staff
+--
+INSERT INTO test
+	(akronym)
+  VALUES
+    ('testin');
---
+++ kmom05/example/index.php
@@ -0,0 +1,19 @@
+<?php
+namespace Ylva\Example;
+
+require __DIR__ . "/config/config.php";
+require __DIR__ . "/vendor/autoload.php";
+require __DIR__ . "/src/Example/Database.php";
+
+
+// Connect to the database
+$db = new Database();
+$db->connect($databaseConfig);
+
+// Collect from the db
+$sql = "SELECT * FROM test;";
+$resultset = $db->executeFetchAll($sql);
+
+var_dump($resultset);
+
+echo 'test';
